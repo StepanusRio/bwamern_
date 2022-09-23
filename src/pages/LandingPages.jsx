7388670bcf502bb5a9322297ae1dc74aa9b79a42
StepanusRio <stepanusriodefaardian@gmail.com>
2022-09-23 18:37:14 +0700

    Complate al footer's

--- src/pages/LandingPages.jsx
+++ src/pages/LandingPages.jsx
@@ -4,6 +4,8 @@ import Hero from "components/Hero";
 import MostPicked from "partials/MostPicked";
 import Categories from 'partials/Categories';
 import landingPage from 'json/landingPage.json'
+import Testimonial from 'partials/Testimonial';
+import Footer from 'partials/Footer';
 
 export class LandingPages extends Component {
   constructor(props) {
@@ -17,6 +19,8 @@ export class LandingPages extends Component {
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
+        <Testimonial data={landingPage.testimonial} />
+        <Footer/>
       </>
     )
   }
